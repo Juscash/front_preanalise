77b8dbb70fc31106033404ff8f0a62b784787256
cledson1996 <cledson.souza@juscash.com.br>
2025-01-23 14:53:58 -0300

    console

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ import GerenciadorPrompt from "./pages/gerenciador_prompt";
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
+console.log("GOOGLE_CLIENT_ID", GOOGLE_CLIENT_ID);
+
+
+
 const PrivateRoutes = [
   { path: "/teste-prompt", component: TestePrompt },
   { path: "/gerenciador-prompt", component: GerenciadorPrompt },
